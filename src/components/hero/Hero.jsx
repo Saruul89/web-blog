d9894f649160db3b2d0db70b9303e6b7f9c25d20
Мөнхсаруул Ганбат <24LP9058@G401-CC0091.local>
2024-09-26 13:07:43 +0800

    complete blog page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 
 export const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
   const [article, setArticle] = useState([]);
 
   const fetchData = async () => {
